78c1d475d41899a2963f62d1319c35f1dbac1505
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-10-11 15:40:23 -0300

    Criando Layout da Activity

--- app/src/main/java/MatheusADSantos/com/github/orgs/MainActivity.kt
+++ app/src/main/java/MatheusADSantos/com/github/orgs/MainActivity.kt
@@ -2,19 +2,14 @@ package MatheusADSantos.com.github.orgs
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import android.widget.Toast
 
-class MainActivity: Activity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Toast.makeText(this, "Bem Vindo(a) ao Orgs!", Toast.LENGTH_LONG).show()
-
-        val view = TextView(this)
-        view.setText("Cesta de Frustas")
-        setContentView(view)
+        setContentView(R.layout.activity_main)
     }
 
 }
