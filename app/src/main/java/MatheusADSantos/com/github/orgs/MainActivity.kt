9197b27173ab2fc6a0e89e9d201bb31fb2e55397
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-10-11 11:59:43 -0300

    Add uma view à Activity dentro do onCreate

--- app/src/main/java/MatheusADSantos/com/github/orgs/MainActivity.kt
+++ app/src/main/java/MatheusADSantos/com/github/orgs/MainActivity.kt
@@ -2,6 +2,8 @@ package MatheusADSantos.com.github.orgs
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity: Activity() {
@@ -9,6 +11,10 @@ class MainActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Toast.makeText(this, "Bem Vindo(a) ao Orgs!", Toast.LENGTH_LONG).show()
+
+        val view = TextView(this)
+        view.setText("Cesta de Frustas")
+        setContentView(view)
     }
 
 }
