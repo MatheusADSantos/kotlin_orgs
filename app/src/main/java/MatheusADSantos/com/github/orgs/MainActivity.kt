e637060b48d1c288fea23795efe9f7f3b6f4aa08
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-10-11 11:46:08 -0300

    Criando cliclo de vida(onCreate) com um popup(Toast) de apresentação ...

--- app/src/main/java/MatheusADSantos/com/github/orgs/MainActivity.kt
+++ app/src/main/java/MatheusADSantos/com/github/orgs/MainActivity.kt
@@ -1,6 +1,14 @@
 package MatheusADSantos.com.github.orgs
 
 import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Bem Vindo(a) ao Orgs!", Toast.LENGTH_LONG).show()
+    }
+
 }
