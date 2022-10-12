b0d072da62928c8b7e172db0fd4ead7db72025c5
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-10-12 11:24:01 -0300

    Buscando as Views do Layout e setando suas strings ...

--- app/src/main/java/MatheusADSantos/com/github/orgs/MainActivity.kt
+++ app/src/main/java/MatheusADSantos/com/github/orgs/MainActivity.kt
@@ -2,6 +2,7 @@ package MatheusADSantos.com.github.orgs
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : Activity() {
@@ -10,6 +11,17 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         Toast.makeText(this, "Bem Vindo(a) ao Orgs!", Toast.LENGTH_LONG).show()
         setContentView(R.layout.activity_main)
+
+        val nome = findViewById<TextView>(R.id.nome)
+        nome.text = "Salada de Frutas"
+
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = "Laranja, Manga e Uva"
+
+        val preco = findViewById<TextView>(R.id.valor)
+        preco.text = "19.99"
+
+
     }
 
 }
