ffaa882547e9acb4d2c700dbc173606706e368b8
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-10-15 00:26:32 -0300

    Add construtor AppCompatActivity

---
+++ app/src/main/java/MatheusADSantos/com/github/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,10 @@
+package MatheusADSantos.com.github.orgs.ui.activity
+
+import MatheusADSantos.com.github.orgs.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormularioProdutoActivity :
+    AppCompatActivity(R.layout.activity_formulario_produto) {
+
+}
--- app/src/main/java/MatheusADSantos/com/github/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/MatheusADSantos/com/github/orgs/ui/activity/MainActivity.kt
@@ -6,15 +6,14 @@ import MatheusADSantos.com.github.orgs.ui.recyclerview.ListaProdutosAdapter
 import android.app.Activity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Toast.makeText(this, "Bem Vindo(a) ao Orgs!", Toast.LENGTH_LONG).show()
-        setContentView(R.layout.activity_main)
-
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ListaProdutosAdapter(
