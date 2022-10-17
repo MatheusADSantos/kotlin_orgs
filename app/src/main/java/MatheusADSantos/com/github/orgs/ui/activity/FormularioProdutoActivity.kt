22e829139b4c474912fa58262cc40a304b8b3545
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-10-17 18:15:21 -0300

    Configurando o Listenner do clique do botão salvar: setOnClickListener{}

--- app/src/main/java/MatheusADSantos/com/github/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/MatheusADSantos/com/github/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -1,10 +1,26 @@
 package MatheusADSantos.com.github.orgs.ui.activity
 
 import MatheusADSantos.com.github.orgs.R
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class FormularioProdutoActivity :
     AppCompatActivity(R.layout.activity_formulario_produto) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val botaoSalvar = findViewById<Button>(R.id.bota_salvar)
+        botaoSalvar.setOnClickListener {
+            val campoNome = findViewById<EditText>(R.id.nome)
+            val nome = campoNome.text.toString()
+            Log.i("FormularioProduto", "onCreate: $nome")
+
+        }
+
+    }
 
 }
