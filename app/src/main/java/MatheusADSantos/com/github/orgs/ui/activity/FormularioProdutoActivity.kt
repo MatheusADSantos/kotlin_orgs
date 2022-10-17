5ac0352ad999814743bbf1ddc4d4c3322d3a5386
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-10-17 18:28:22 -0300

    Criando um produto no formulário; E transformando a classe Produto num 'data class'

--- app/src/main/java/MatheusADSantos/com/github/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/MatheusADSantos/com/github/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -1,11 +1,13 @@
 package MatheusADSantos.com.github.orgs.ui.activity
 
 import MatheusADSantos.com.github.orgs.R
+import MatheusADSantos.com.github.orgs.model.Produto
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import java.math.BigDecimal
 
 class FormularioProdutoActivity :
     AppCompatActivity(R.layout.activity_formulario_produto) {
@@ -15,9 +17,23 @@ class FormularioProdutoActivity :
 
         val botaoSalvar = findViewById<Button>(R.id.bota_salvar)
         botaoSalvar.setOnClickListener {
+
             val campoNome = findViewById<EditText>(R.id.nome)
             val nome = campoNome.text.toString()
-            Log.i("FormularioProduto", "onCreate: $nome")
+
+            val campoDescricao = findViewById<EditText>(R.id.descricao)
+            val descricao = campoDescricao.text.toString()
+
+            val campovalor = findViewById<EditText>(R.id.valor)
+            val valorTexto = campovalor.text.toString()
+            val valor = if (valorTexto.isBlank()) {
+                BigDecimal.ZERO
+            } else {
+                BigDecimal(valorTexto)
+            }
+
+            val produto = Produto(nome = nome, descricao = descricao, valor = valor)
+            Log.i("FormularioProduto", "onCreate: $produto")
 
         }
 
