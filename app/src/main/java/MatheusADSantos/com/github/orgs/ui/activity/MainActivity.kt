80f27f7727064f3d780fa2fa2bf9f2d81facd589
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-10-17 23:57:46 -0300

    Iniciando Activity: Através do FAB, Intent e starAcitivity(intent) https://material.io/components/buttons-floating-action-button

--- app/src/main/java/MatheusADSantos/com/github/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/MatheusADSantos/com/github/orgs/ui/activity/MainActivity.kt
@@ -4,10 +4,12 @@ import MatheusADSantos.com.github.orgs.R
 import MatheusADSantos.com.github.orgs.model.Produto
 import MatheusADSantos.com.github.orgs.ui.recyclerview.ListaProdutosAdapter
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
@@ -23,21 +25,30 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                     nome = "teste",
                     descricao = "desc teste",
                     valor = "9.99".toBigDecimal()
-                ),Produto(
+                ),
+                Produto(
                     nome = "teste2",
                     descricao = "desc2 teste",
                     valor = "29.99".toBigDecimal()
-                ),Produto(
+                ),
+                Produto(
                     nome = "teste3",
                     descricao = "desc3 teste",
                     valor = "39.99".toBigDecimal()
-                ),Produto(
+                ),
+                Produto(
                     nome = "teste4",
                     descricao = "desc4 teste",
                     valor = "49.99".toBigDecimal()
                 ),
             )
         )
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab_action_add)
+        fab.setOnClickListener {
+            val intent = Intent(this, FormularioProdutoActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 
