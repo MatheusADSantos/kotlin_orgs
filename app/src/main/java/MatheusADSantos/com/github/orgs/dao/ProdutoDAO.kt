d3fcacbd081b5ac5b3f003d4924b8f8eaf612497
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-10-25 11:19:08 -0300

    Inserindo ImageView no layout do item da Lista

--- app/src/main/java/MatheusADSantos/com/github/orgs/dao/ProdutoDAO.kt
+++ app/src/main/java/MatheusADSantos/com/github/orgs/dao/ProdutoDAO.kt
@@ -1,6 +1,7 @@
 package MatheusADSantos.com.github.orgs.dao
 
 import MatheusADSantos.com.github.orgs.model.Produto
+import java.math.BigDecimal
 
 class ProdutoDAO {
 
@@ -13,6 +14,14 @@ class ProdutoDAO {
     }
 
     companion object {
-        private val produtos = mutableListOf<Produto>()
+        private val produtos = mutableListOf<Produto>(
+            Produto(
+                nome = "Salada de frutas",
+                descricao = "Laranja, maçãs e uva",
+                valor = BigDecimal("19.83")
+            )
+        )
     }
+
+
 }
